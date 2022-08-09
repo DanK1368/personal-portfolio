cf4881bce2106b6e96735a5c149aac791c4e67a6
DanK1368 <danielkoett@gmail.com>
2022-08-09 17:04:19 +0200

    added a skills array to map through in the skills component

---
+++ constants/skills.js
@@ -0,0 +1,69 @@
+import cssIcon from "../public/assets/icons/css.png";
+import htmlIcon from "../public/assets/icons/html5.png";
+import sassIcon from "../public/assets/icons/sass.png";
+import tailwindIcon from "../public/assets/icons/tailwind.png";
+import javascriptIcon from "../public/assets/icons/js.png";
+import reactIcon from "../public/assets/icons/reactjs.png";
+import djangoIcon from "../public/assets/icons/django.png";
+import pythonIcon from "../public/assets/icons/python.png";
+import nextIcon from "../public/assets/icons/nextjs.png";
+import typescriptIcon from "../public/assets/icons/typescript.png";
+
+export const frontEndSkills = [
+  {
+    id: 1,
+    name: "HTML5",
+    icon: htmlIcon,
+  },
+  {
+    id: 2,
+    name: "CSS",
+    icon: cssIcon,
+  },
+  {
+    id: 3,
+    name: "SASS",
+    icon: sassIcon,
+  },
+  {
+    id: 4,
+    name: "Tailwind",
+    icon: tailwindIcon,
+  },
+  {
+    id: 5,
+    name: "Javascript",
+    icon: javascriptIcon,
+  },
+  {
+    id: 6,
+    name: "React.Js",
+    icon: reactIcon,
+  },
+];
+
+export const backEndSkills = [
+  {
+    id: 1,
+    name: "Python",
+    icon: pythonIcon,
+  },
+  {
+    id: 2,
+    name: "Django",
+    icon: djangoIcon,
+  },
+];
+
+export const learning = [
+  {
+    id: 1,
+    name: "Next.js",
+    icon: nextIcon,
+  },
+  {
+    id: 2,
+    name: "Typescript",
+    icon: typescriptIcon,
+  },
+];
