9bf22f63691cf99ba57b4b27e10ab8fd5e46a483
DanK1368 <danielkoett@gmail.com>
2022-08-09 17:00:27 +0200

    updated props as an array is used instead

--- components/ProjectItem.js
+++ components/ProjectItem.js
@@ -1,24 +1,46 @@
 import Image from "next/image";
-import Link from "next/link";
 
-const ProjectItem = ({ title, bgImage, projectUrl, techDescription }) => {
+const ProjectItem = ({ image, title, tags, source, liveSite }) => {
   return (
-    <div className=" relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-accent">
+    <div className=" relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gray-800 ">
       <Image
         className=" rounded-xl group-hover:opacity-10  "
-        src={bgImage}
+        src={image}
         alt=""
       />
       <div className=" hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] ">
-        <h3 className=" text-2xl text-white tracking-wider text-center ">
+        <h3 className=" text-xl text-white tracking-wider text-center lg:text-3xl">
           {title}
         </h3>
-        <p className=" pb-4 pt-2 text-white text-center ">{techDescription}</p>
-        <Link href="/">
-          <p className=" text-center py3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer hover:bg-primary hover:text-white">
-            Check Me Out
-          </p>
-        </Link>
+        <div className=" flex justify-center items-center gap-2 py-5 ">
+          {tags.map((tag, idx) => (
+            <p
+              key={idx}
+              className=" badge badge-secondary text-xs w-max flex justify-center items-center "
+            >
+              {tag}
+            </p>
+          ))}
+        </div>
+
+        <div className=" flex justify-center items-center gap-4">
+          <a
+            href={source}
+            target="_blank"
+            rel="noreferrer"
+            className="badge badge-info text-white hover:scale-110"
+          >
+            Source
+          </a>
+          <a
+            href={liveSite}
+            target="_blank"
+            rel="noreferrer"
+            className="badge badge-info text-white hover:scale-110"
+          >
+            Live Site
+          </a>
+        </div>
       </div>
     </div>
   );
