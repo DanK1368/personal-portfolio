2f438344618bfd848529dd6bd03ee4c3a7eed338
DanK1368 <danielkoett@gmail.com>
2022-08-09 17:04:32 +0200

    added a projects array to map through in the projects component

---
+++ constants/projects.js
@@ -0,0 +1,63 @@
+import dropshopImg from "../public/assets/dropshop-project.png";
+import skilledImg from "../public/assets/skilled-screenshot.png";
+import todoImg from "../public/assets/todo-screenshot.png";
+import podImg from "../public/assets/pod-screenshot.png";
+import singlePageImg from "../public/assets/single-page-screenshot.png";
+import splitterImg from "../public/assets/splitter-screenshot.png";
+
+export const projects = [
+  {
+    id: 0,
+    title: "DropShop Full-Stack Project for SIT Academy",
+    description: "",
+    image: dropshopImg,
+    tags: ["React.js", "Framer Motion", "Django REST"],
+    source: "https://github.com/DanK1368/dropshop/tree/master",
+    liveSite: "https://dropshop.propulsion-learn.ch/",
+  },
+  {
+    id: 1,
+    title: "Single-Page-Design Portfolio",
+    description: "",
+    image: singlePageImg,
+    tags: ["React.js", "Tailwind CSS", "Framer Motion", "RWD"],
+    source: "https://github.com/DanK1368/single-page-design-portfolio",
+    liveSite: "https://single-page-design-application.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Skilled Landing Page",
+    description: "",
+    image: skilledImg,
+    tags: ["React.js", "SASS", "RWD"],
+    source: "https://github.com/DanK1368/skilled-elearning-landing-page",
+    liveSite: "https://dank1368.github.io/skilled-elearning-landing-page/",
+  },
+  {
+    id: 3,
+    title: "Splitter Bill Calculator ",
+    description: "",
+    image: splitterImg,
+    tags: ["React.js", "SASS", "RWD"],
+    source: "https://github.com/DanK1368/tip-calculator-app",
+    liveSite: "https://dank1368.github.io/tip-calculator-app/",
+  },
+  {
+    id: 4,
+    title: "Todo List ",
+    description: "",
+    image: todoImg,
+    tags: ["Vanilla JS", "SASS", "RWD"],
+    source: "https://github.com/DanK1368/todo__app",
+    liveSite: "https://dank1368.github.io/todo__app/",
+  },
+  {
+    id: 5,
+    title: "Pod Landing Page ",
+    description: "",
+    image: podImg,
+    tags: ["Vanilla JS", "SASS", "RWD"],
+    source: "https://github.com/DanK1368/pod_request_access_landing_page",
+    liveSite: "https://dank1368.github.io/pod_request_access_landing_page/",
+  },
+];
