9c60854a7e34703dfdc410bf26c325c20659c602
DanK1368 <danielkoett@gmail.com>
2022-08-14 23:15:31 +0200

    seo optimization added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,44 @@
-import '../styles/globals.css'
+import SEO from "@bradgarropy/next-seo";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <SEO
+        title="DK | Full Stack Developer"
+        description="A personal portfolio showing projects I worked on"
+        keywords={[
+          "website",
+          "web developer",
+          "developer",
+          "portfolio",
+          "front-end",
+          "full-stack",
+          "backend",
+          "react",
+          "python",
+          "django",
+          "api",
+          "javascript",
+          "html",
+          "css",
+          "tailwind",
+        ]}
+        icon="/favicon.ico"
+        facebook={{
+          image: "/screenshot.png",
+          url: "https://danielkoett.xyz",
+          type: "website",
+        }}
+        twitter={{
+          image: "/screenshot.png",
+          site: "@danielkoett",
+          card: "summary",
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
