5238b092f2cf0520a9ff79ebb4078de7fe337724
DanK1368 <danielkoett@gmail.com>
2022-08-15 13:03:13 +0200

    favicon image changed

--- pages/_app.js
+++ pages/_app.js
@@ -24,7 +24,7 @@ function MyApp({ Component, pageProps }) {
           "css",
           "tailwind",
         ]}
-        icon="/favicon.ico"
+        icon="/favicon.png"
         facebook={{
           image: "/screenshot.png",
           url: "https://danielkoett.xyz",
