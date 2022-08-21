46993f2730318b7e7b2bb2682e38610425784919
DanK1368 <danielkoett@gmail.com>
2022-08-21 14:27:21 +0200

    removed drag feature

--- components/ChatBot.js
+++ components/ChatBot.js
@@ -4,14 +4,11 @@ import { motion } from "framer-motion";
 
 const ChatBot = () => {
   return (
-    <motion.div
-      drag
-      className=" w-full sticky top-[650px] z-50 min-h-[10vh] flex items-center justify-end pr-10 xs:top[750px]"
-    >
+    <div className=" w-full sticky top-[600px] z-50 min-h-[10vh] flex items-center justify-end pr-10 xs:top[750px]">
       <div className=" animate-bounce right-0 max-w-[40px] ">
         <Image src={robotSvg} alt="" />
       </div>
-    </motion.div>
+    </div>
   );
 };
 export default ChatBot;
